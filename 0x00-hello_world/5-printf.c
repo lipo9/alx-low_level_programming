63ca44938c03abd41bdd2d5beed8645a477b85d4
Abderrahim Libourki <abdolibourki@gmail.com>
2022-09-08 22:05:02 +0100

    5. Hello, printf
    This is a C program that prints exactly with proper grammar,
         but the outcome is a piece of art,, followed by a new line
    
    	-Use the function printf
    	-You are not allowed to use the function puts
    	-Your program should return 0
    	-Your program should compile without warning when using the -Wall gcc
    	 option

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - This is the function that is called at the program startup
+ * @void: takes no parameters
+ * Description: The main function is the first function tha is executed
+ * in the program when the execution is started
+ * Return: retuns 0 (int value)
+  */
+int main(void)
+{
+	/**
+	 * Description: using the built-in function "printf"
+	 * to print exactly "with proper grammar, but the outcome is a piece
+	 * of art," followed by a new line
+	 *
+	 * printf - sends a formatted output to stdout
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
