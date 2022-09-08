b5d223e27465582de4ed9993abf97076a73765a3
Abderrahim Libourki <abdolibourki@gmail.com>
2022-09-08 21:55:46 +0100

    4. Hello, puts
    This is  a C program that prints exactly "Programming is like building
    a multilingual puzzle, followed by a new line.
    	-Use the function puts
    	-You are not allowed to use printf
    	-Your program should end with the value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - This is the function that is called at the program startup
+ * @void: takes no parameters
+ * Description: The main function is the first function tha is executed
+ * in the program when the execution is started
+ * Return: retuns 0 (int value)
+  */
+int main(void)
+{
+	/**
+	 * Description: using the built-in function "puts"
+	 * to print exactly "Programming is like building a multilingual puzzle
+	 *
+	 * puts - writes a string to stdout wothout the null character, followed
+	 * by a new line
+	 */
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
