a33c7c63c96690eb89595e47a843cc0e69a80777
Abderrahim Libourki <abdolibourki@gmail.com>
2022-09-09 20:29:31 +0100

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
    This is a program that prints the alphabet in lowercase, followed by a new line.
    
    	- Only use the putchar function
    	 (every other function (printf, puts, etc…) is forbidden)
    	- All the code should be in the main function
    	- Only use putchar twice in your code

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - This is the function that is called at the program startup
+ * Description: The main function is the first function tha is executed
+ * in the program when the execution is started
+ * Return: retuns 0 (int value)
+  */
+int main(void)
+{
+	/*
+	 * This program prints the alphabet in lowercase, followed by a new line
+	 *
+	 */
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
