e6ab64e68a4b88e8d0117bdf3b5bdad8c96b056e
Abderrahim Libourki <abdolibourki@gmail.com>
2022-09-13 18:32:31 +0100

    8. I'm federal agent Jack Bauer, and today is the longest day of my life
    This program contains a function that prints every minute of the day of
    Jack Bauer, starting from 00:00 to 23:59.
    	- Prototype: void jack_bauer(void);

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  * jack_bauer - This is a function that prints
+  * every minute of the da of Jack Bauer
+  * Return: void
+  */
+
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2;
+
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 <= 9; h2++)
+		{
+			if (h1 == 2 && h2 == 4)
+				return;
+			m1 = 0;
+			while (m1 <= 5)
+			{
+				m2 = 0;
+				while (m2 <= 9)
+				{
+					_putchar(h1 + '0');
+					_putchar(h2 + '0');
+					_putchar(':');
+					_putchar(m1 + '0');
+					_putchar(m2 + '0');
+					_putchar('\n');
+					m2++;
+				}
+				m1++;
+			}
+		}
+	}
+}
