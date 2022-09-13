9037f8f251b4c6c93a9658ae689d08f255eb59e9
Abderrahim Libourki <abdolibourki@gmail.com>
2022-09-13 19:37:32 +0100

    10. a + b
    This program contains a function that adds two integers and returns the result.
    	- Prototype: int add(int, int);

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * add - This is a function that add two numbers
+  * and returns the result
+  * @a: integer number
+  * @b: integer number
+  * Return: the sum of a and b
+  */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
