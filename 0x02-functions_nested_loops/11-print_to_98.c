a29eddc43c6cc931b761e6f44ef41a154e8b2956
Abderrahim Libourki <abdolibourki@gmail.com>
2022-09-13 21:12:02 +0100

    11. 98 Battery Street, the OG
    This program contains a function that prints all natural numbers from n to 98,
         followed by a new line.
    	- Prototype: void print_to_98(int n);
    	- Numbers are separated by a comma, followed by a space
    	- Numbers are printed in order
    	- The first printed number is the number passed to the function
    	- The last printed number is 98
    	- Use of the standard library (allowed)

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - this function prints number from n to 98
+ * @n: integer number
+ * Return: void
+  */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	else if (n < 98)
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	printf("98\n");
+}
