0c4683a1d6ae35871318708f892768ffed19e590
Abderrahim Libourki <abdolibourki@gmail.com>
2022-10-05 02:20:29 +0100

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,15 +3,15 @@
 /**
  * free_grid - frees a 2 dimensional grid previously created by alloc_grid
  * @grid: pointer to two dim array
- * @height: height of the array
+ * @h: h of the array
  * Return: void
  */
 
-void free_grid(char **grid, char height)
+void free_grid(char **grid, char h)
 {
 	int i = 0;
 
-	for (i = 0; i < height; i++)
+	for (i = 0; i < h; i++)
 		free(*(grid + i));
 	free(grid);
 }
@@ -20,36 +20,37 @@ void free_grid(char **grid, char height)
 /**
  * strtow - splits a string into words.
  * @str: string.
- *
  * Return: pointer of an array of integers
  */
 
 char **strtow(char *str)
 {
 	char **grid;
-	unsigned int c, height, i, j, k;
+	unsigned int c, h, i, j, k;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (c = height = 0; str[c] != '\0'; c++)
-		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			height++;
-	grid = malloc((height + 1) * sizeof(char *));
-	if (grid == NULL || height == 0)
+	for (c = h = 0; *(str + c); c++)
+		if (*(str + c) != ' ' && (*(str + c + 1) == ' '
+		|| *(str + c + 1) == '\0'))
+			h++;
+	grid = malloc((h + 1) * sizeof(char *));
+	if (grid == NULL || h == 0)
 	{
 		free(grid);
 		return (NULL);
 	}
-	for (i = k = 0; i < height; i++)
+	for (i = k = 0; i < h; i++)
 	{
-		for (c = k; str[c] != '\0'; c++)
+		for (c = k; *(str + c); c++)
 		{
-			if (str[c] == ' ')
+			if (*(str + c) == ' ')
 				k++;
-			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			if (*(str + c) != ' ' && (*(str + c + 1) == ' '
+			|| *(str + c + 1) == '\0'))
 			{
-				grid[i] = malloc((c - k + 2) * sizeof(char));
-				if (grid[i] == NULL)
+				*(grid + i) = malloc((c - k + 2) * sizeof(char));
+				if (*(grid + i) == NULL)
 				{
 					free_grid(grid, i);
 					return (NULL);
@@ -58,8 +59,8 @@ char **strtow(char *str)
 			}
 		}
 		for (j = 0; k <= c; k++, j++)
-			grid[i][j] = str[k];
-		grid[i][j] = '\0';
+			*(*(grid + i) + j) = *(str + k);
+		*(*(grid + i) + j) = '\0';
 	}
 	grid[i] = NULL;
 	return (grid);
