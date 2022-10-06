78fcbcdb68688529484cb86df0ed955be74a154b
Abderrahim Libourki <abdolibourki@gmail.com>
2022-10-06 16:41:38 +0100

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,43 +1,119 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
- * main - entry of the program
- * Description: a program that multiplies two numbers
- * @argc: number of the program arguments
- * @argv: array of pointers to the commandline arguments
- * Return: EXIT_SUCCESS
+ * iszero - determines if any number is zero
+ * @argv: array of arguments
+ * Return: void
  */
-
-int main(int __attribute__((__unused__)) argc, char **argv)
+void iszero(char *argv[])
 {
-	int size_1 = 0, size_2 = 0;
+	int i, z1 = 1, z2 = 1;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	while (*(argv[1] + size_1))
-	{
-		if (*(argv[1] + size_1) < 48 || *(argv[1] + size_1) > 57)
+	for (i = 0; *(argv[1] + i); i++)
+		if (*(argv[1] + i) != '0')
 		{
-			printf("Error\n");
-			exit(98);
+			z1 = 0;
+			break;
 		}
-		size_1++;
-	}
-	while (*(argv[2] + size_2))
-	{
-		if (*(argv[2] + size_2) < 48 || *(argv[2] + size_2) > 57)
+	for (i = 0; *(argv[2] + i); i++)
+		if (*(argv[2] + i) != '0')
 		{
-			printf("Error\n");
-			exit(98);
+			z2 = 0;
+			break;
 		}
-		size_2++;
+	if (z1 == 1 || z2 == 1)
+	{
+		printf("0\n");
+		exit(0);
 	}
-
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (EXIT_SUCCESS);
 }
 
+/**
+ * init_arr - init an array to zeros
+ * @ar: char array.
+ * @n: len of the char array.
+ * Return: points to an array of chars
+ */
+char *init_arr(char *ar, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+		*(ar + i) = '0';
+	*(ar + n) = '\0';
+	return (ar);
+}
+
+/**
+ * check_number - gets len of the number
+ * and checks if number is in base 10.
+ * @argv: array of arguments
+ * @n: row of the array
+ * Return: len of the given number
+ */
+int check_number(char *argv[], int n)
+{
+	int len;
+
+	for (len = 0; *(argv[n] + len); len++)
+		if (!isdigit(*(argv[n] + len)))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (len);
+}
+
+/**
+ * main - a program that multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: EXIT_SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+int len_1, len_2, res_len, add, addl, i, j, k, ca;
+char *res;
+
+if (argc != 3)
+	printf("Error\n"), exit(98);
+len_1 = check_number(argv, 1), len_2 = check_number(argv, 2);
+iszero(argv), res_len = len_1 + len_2, res = malloc(res_len + 1);
+if (res == NULL)
+	printf("Error\n"), exit(98);
+res = init_arr(res, res_len);
+k = res_len - 1, i = len_1 - 1, j = len_2 - 1, ca = addl = 0;
+for (; k >= 0; k--, i--)
+{
+	if (i < 0)
+	{
+		if (addl > 0)
+		{
+			add = (res[k] - '0') + addl;
+			if (add > 9)
+				res[k - 1] = (add / 10) + '0';
+			res[k] = (add % 10) + '0';
+		}
+		i = len_1 - 1, j--, addl = 0, ca++, k = res_len - (1 + ca);
+	}
+	if (j < 0)
+	{
+		if (res[0] != '0')
+			break;
+		res_len--, free(res), res = malloc(res_len + 1);
+		res = init_arr(res, res_len);
+		k = res_len - 1, i = len_1 - 1, j = len_2 - 1, ca = addl = 0;
+	}
+	if (j >= 0)
+	{
+		add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (res[k] - '0') + addl;
+		addl = add / 10, res[k] = (add % 10) + '0';
+	}
+}
+printf("%s\n", res), free(res);
+return (EXIT_SUCCESS);
+}
